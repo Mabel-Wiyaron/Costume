27a6b2cabe58f039ea1fd4404ae7675af0edcb74
Saujana Shafi <hello@saujanashafi.com>
2026-07-23 10:09:04 +0700

    refactor: Independent session

--- Costume/LanguageModel/LanguageModelProtocol.swift
+++ Costume/LanguageModel/LanguageModelProtocol.swift
@@ -9,10 +9,12 @@ import Foundation
 import FoundationModels
 
 protocol LanguageModelProtocol {
+    var instructions: String { get set }
+    var temperature: Double { get set }
+
+    init(instructions: String, temperature: Double)
+
     func invoke(for message: String) async throws -> String
     func generate<Content>(content type: Content.Type, for message: String)
-        async throws -> Content where Content: Generable
-    // TODO: Refine this reset method.
-    // Might need mutating
-    func reset()
+        async throws -> Content where Content: Generable & Decodable
 }
